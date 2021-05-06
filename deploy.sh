21aea5e9bacc0803a679a0e1ccd214ab7fc845ff
xuyan0 <xuyan0@aixuexi.com>
2021-05-06 11:50:58 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HuaYuan92/notebook.git main:blogs
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:HuaYuan92/notebook.git master:gh-pages
 
 cd -
