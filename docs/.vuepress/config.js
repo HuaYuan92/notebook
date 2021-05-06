486fba305f25d1f92170ea7eef2acb39fd7971a9
xuyan0 <xuyan0@aixuexi.com>
2021-05-06 19:13:22 +0800

    feat: 初步迁移

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,8 +9,13 @@ module.exports = {
     logo: '/logo.png',
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
+      { text: '算法', link: '/algorithm/' },
+      { text: 'vue', link: '/vue/' },
       { text: 'github', link: 'https://github.com/HuaYuan92/notebook' },
-    ]
-  }
+    ],
+    sidebar: 'auto',
+    smoothScroll: true,
+    lastUpdated: 'Last Updated',
+    displayAllHeaders: true
+  },
 }
